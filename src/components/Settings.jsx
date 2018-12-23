0fc7fcb8a6027510e154849c53c0a8361d9b3020
Sergii Bomko <aquila.ua@gmail.com>
2018-12-23 15:07:57 +0100

    adds settings component

---
+++ src/components/Settings.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+class Settings extends Component {
+  render() {
+    return (
+      <Container fluid>
+        Settings
+      </Container>
+    );
+  }
+}
+
+export default Settings;
