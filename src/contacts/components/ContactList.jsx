b58677761bd249ccfa47c2155affb5c43eee9a18
Sergii Bomko <aquila.ua@gmail.com>
2018-12-31 22:27:18 +0100

    adds contact invite form

--- src/contacts/components/ContactList.jsx
+++ src/contacts/components/ContactList.jsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+
+import ContactInvite from './ContactInvite';
 
 class ContactList extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    contacts: PropTypes.object,
+  };
+
   render() {
     return (
-      <div>tesssss</div>
+      <div className='pt-3'>
+        <h5>
+          All
+          <ContactInvite />
+        </h5>
+      </div>
     );
   }
 }
 
-export default ContactList;
+export default compose(
+  connect((state) => {
+    const { contacts } = state || {};
+    return { contacts };
+  })
+)(ContactList);
