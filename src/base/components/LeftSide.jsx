459938ba6b4e183f01c2d38e5b6488f0b1f023c2
Sergii Bomko <aquila.ua@gmail.com>
2018-12-31 22:17:51 +0100

    adds Contact list components

--- src/base/components/LeftSide.jsx
+++ src/base/components/LeftSide.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 
 import Account from './../../account/components/Account';
+import ContactList from './../../contacts/components/ContactList';
 
 export default function LeftSide() {
   return (
     <Fragment>
       <Account />
+      <ContactList />
     </Fragment>
   )
 }
