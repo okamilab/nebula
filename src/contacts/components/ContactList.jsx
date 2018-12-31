5de646817e608d10925c6f7a39a0f36e94ab8997
Sergii Bomko <aquila.ua@gmail.com>
2018-12-31 22:18:06 +0100

    adds Contact list components

---
+++ src/contacts/components/ContactList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ContactList extends Component {
+  render() {
+    return (
+      <div>tesssss</div>
+    );
+  }
+}
+
+export default ContactList;
