fb1217ee07522dfae2fb355dd3374bdd7a63fcff
Sergii Bomko <aquila.ua@gmail.com>
2019-01-01 22:11:43 +0100

    adds contact page

---
+++ src/contacts/components/ContactRequest.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button, Modal, ModalHeader,
+  ModalBody, ModalFooter
+} from 'reactstrap';
+
+class ContactRequest extends Component {
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    onAccept: PropTypes.func.isRequired,
+    onDecline: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    const { value, onAccept, onDecline } = this.props;
+
+    return (
+      <div className='text-truncate' onClick={this.toggle}>
+        {value}
+        <Modal isOpen={this.state.modal} centered>
+          <ModalHeader>Received contact</ModalHeader>
+          <ModalBody style={{ wordWrap: 'break-word' }}>
+            Do you want to accept request from {value}?
+          </ModalBody>
+          <ModalFooter>
+            <Button color='light' onClick={() => { onDecline(value); this.toggle(); }}>Decline</Button>
+            <Button color='primary' onClick={() => { onAccept(value); this.toggle(); }}>Accept</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ContactRequest;
